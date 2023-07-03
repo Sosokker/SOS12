0fbc8f5273257c63da2149dd0668397f65962041
Yanatchara JERAJA <yanatchara.j@ku.th>
2023-07-03 15:01:38 +0700

    added FAQ

--- src/constants/index.js
+++ src/constants/index.js
@@ -47,24 +47,69 @@ const menu = [
 ];
 
 const faqs = [
-  {
-    section: 'Python',
-    question: 'How do I install Python?',
-    answer: 'To install Python, you can follow the step-by-step tutorial in this guide: [Python Installation Tutorial](https://www.example.com/python-installation-tutorial). It will walk you through the process of installing Python on your system.',
-    link: 'https://www.example.com/python-installation-tutorial'
+   {
+    section: 'About SKE',
+    question: 'What is SKE?',
+    answer: 'Software and Knowledge Engineering or SKE (pronounce "ski") for short, is a branch in a Computer Engineering, Kasetsart University. Our branch is also a part of the international Undergraduate Program (IUP).'
   },
   {
-    section: 'General',
-    question: 'How do I brush my teeth?',
-    answer: 'To brush your teeth, follow these steps:\n1. Wet your toothbrush with water.\n2. Apply toothpaste to the bristles.\n3. Hold the toothbrush at a 45-degree angle to your gums.\n4. Brush gently in circular motions for 2 minutes.\n5. Spit out the toothpaste and rinse your mouth with water.',
-    link: null
+    section: 'About SKE',
+    question: 'What are the required courses for this branch?',
+    answer: 'For freshmen it is required to learn the general engineering courses such as "Engineering Mathematics I", "General Physics I" and "Physics Lab I". You will also need to apply for essential Computer Engineering courses such as, "Computer Programming I" and "Discrete Mathematics and Algebra". For Computer Programming I, you will get to learn the fundamentals of programming with Python. For Discrete Mathematics and Algebra, you will learn about the basic of discrete mathematics and algebra, which is essential for programming.'
   },
   {
-    section: 'Miscellaneous',
-    question: 'What is the capital of France?',
-    answer: 'The capital of France is Paris.'
-  }
-];
+    section: 'About SKE',
+    question: 'Are the courses difficult?',
+    answer: 'The difficulty of each course varies on each learner, it depends on whether you have basics or not. You might need to adjust and may have to work harder than others, also try to learn by yourself and practice doing exercises.'
+  },
+  {
+    section: 'About SKE',
+    question: 'Is a computer requires? What about specifications?',
+    answer: 'A computor is required though it is REALLY recommended to have a laptop. Portable computers will make your life easier and should always be taken to university. For windows, you should pick a laptop with medium specs such as, Core i5 or Ryzen 5 CPU, 8GB RAM, 512 HDD. Make sure to get teh latest generation of CPU to have faster performance. For Mac, it is recommended to get at least M1 CPU.'
+  },
+  {
+    section: 'About SKE',
+    question: 'When does the course start and what to prepare?',
+    answer: 'The first semester\'s start date is 7th August. Though there will be events before that day like SKE tutorial camp'
+  },
+  {
+    section: 'About SOS12 Camp',
+    question: 'What is SOS Camp?',
+    answer: 'SOS Camp is a preparation camp for freshmen to gain knowledge about the programming. It is also a camp to help you prepare for the first semester. The camp will be held on 18th-21th July.'
+  },
+  {
+    section: 'About SOS12 Camp',
+    question: 'What programming languages will be covered in the camp?',
+    answer: 'This camp will cover Python.'
+  },
+  {
+    section: 'About SOS12 Camp',
+    question: 'Do I need prior programming experience to attend the camp?',
+    answer: 'No, you do not need any prior programming experience to attend the camp. The camp will start from the very basics of programming.'
+  },
+  {
+    section: 'Programming',
+    question: 'What programming concepts will be taught during the camp?',
+    answer: 'The programming concepts that will be taught during the camp are variables, data types, operators, conditional statements, loops, and functions.'
+  },
+  {
+    section: 'Programming',
+    question: 'Will the camp cover both theoretical and practical aspects of programming?',
+    answer: 'Yes, the camp will cover both theoretical and practical aspects of programming. The camp will start with a lecture and then you will be given exercises to practice what you have learned.'
+  },
+  {
+    section: 'Programming',
+    question: 'Can I bring my own laptop or do I need to use university computers',
+    answer: 'You can bring your own laptop. However, if you do not have a laptop, you can use the university computers.'
+  },
+  {
+    section: 'Programming',
+    question: 'What software do I need to install on my laptop?',
+    answer: 'You will need to install Python 3.9.6 and PyCharm Community Edition 2021.1.3.'
+  },
+  ]
+
+
 
 const socialMediaData = [
   {
