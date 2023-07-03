cd292a5dc0d38e1a5a161618eb5901875606a619
sosokker <sirin_29@hotmail.co.th>
2023-07-03 15:40:17 +0700

    Wrong import file's name

--- src/components/index.js
+++ src/components/index.js
@@ -6,7 +6,7 @@ import CanvasLoader from "./Loader";
 import Faq from './Faq';
 import Problems from './Problems';
 import Preproblems from './Preproblems';
-import SocialMedia from './Socialmedia';
+import SocialMedia from './SocialMedia';
 
 export {
     Navbar,
