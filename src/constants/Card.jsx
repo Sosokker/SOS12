739646d4348ce7f828e27079446219eaa303b098
Sirin Puenggun <sirin_29@hotmail.co.th>
2023-07-16 19:31:50 +0700

    try fix image missing on deploy

--- src/constants/Card.jsx
+++ src/constants/Card.jsx
@@ -3,70 +3,70 @@ const cards = [
         'title': 'Data type',
         'describe': 'Day 1',
         'link': 'https://elabsheet.org/elab/taskpads/show/iln6rkv0ch/',
-        'img': '../../public/images/gif/_1.jpg',
+        'img': '/images/gif/_1.jpg',
         'id': 1
     },
     {
         'title': 'Boolean',
         'describe': 'Day 1',
         'link': 'https://elabsheet.org/elab/taskpads/show/558n9h46d9/',
-        'img': '../../public/images/gif/_2.jpg',
+        'img': '/images/gif/_2.jpg',
         'id': 2
     },
     {
         'title': 'Operator 01 (Area)',
         'describe': 'Day 1',
         'link': 'https://elabsheet.org/elab/taskpads/show/l2uoxc77z9/',
-        'img': '../../public/images/gif/_3.jpg',
+        'img': '/images/gif/_3.jpg',
         'id': 3
     },
     {
         'title': 'Operator 02 (Radius)',
         'describe': 'Day 1',
         'link': 'https://elabsheet.org/elab/taskpads/show/dzm24rq6hp/',
-        'img': '../../public/images/gif/_4.jpg',
+        'img': '/images/gif/_4.jpg',
         'id': 4
     },
     {
         'title': 'Variables',
         'describe': 'Day 1',
         'link': 'https://elabsheet.org/elab/taskpads/show/jr070sqaqp/',
-        'img': '../../public/images/gif/_5.jpg',
+        'img': '/images/gif/_5.jpg',
         'id': 5
     },
     {
         'title': 'Numbers, Casting, String',
         'describe': 'Day 1',
         'link': 'https://elabsheet.org/elab/taskpads/show/qkea3sxyo0/',
-        'img': '../../public/images/gif/_6.jpg',
+        'img': '/images/gif/_6.jpg',
         'id': 6
     },
     {
         'title': 'Strings',
         'describe': 'Day 1',
         'link': 'https://elabsheet.org/elab/taskpads/show/npxunwm0n5/',
-        'img': '../../public/images/gif/_7.jpg',
+        'img': '/images/gif/_7.jpg',
         'id': 7
     },
     {
         'title': 'Strings slicing',
         'describe': 'Day 1',
         'link': 'https://elabsheet.org/elab/taskpads/show/h62ity5o8f/',
-        'img': '../../public/images/gif/_8.jpg',
+        'img': '/images/gif/_8.jpg',
         'id': 8
     },
     {
         'title': 'Computation',
         'describe': 'Day 1',
         'link': 'https://elabsheet.org/elab/taskpads/show/0537fkytq9/',
-        'img': '../../public/images/gif/_9.jpg',
+        'img': '/images/gif/_9.jpg',
         'id': 9
     },
     {
         'title': 'Comment',
         'describe': 'Day 1',
         'link': 'https://elabsheet.org/elab/taskpads/show/xj450gjw5y/',
-        'img': '../../public/images/gif/_10.jpg',
+        'img': '/images/gif/_10.jpg',
         'id': 10
     }
 ]
