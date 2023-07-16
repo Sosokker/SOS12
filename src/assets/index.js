b9c13a425f8040de5f2cbf70ee8953f2b973ff5c
Krittin SETDHAVANICH <krittin.set@ku.th>
2023-07-16 13:50:10 +0700

    deleted unused image

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,39 +1,20 @@
 import logo from "./logo.svg";
 import problems from "./problems.png";
-import guide from "./guide.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 import contacts_pic from "./contacts.png"
 import faq_pic from "./faq.png"
-
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
-
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
-
 import discord_icon from './Discord.png';
 import instagram_icon from './instagram.png';
 import facebook_icon from './facebook.png';
 
 export {
   logo,
-  guide,
   problems,
   github,
   menu,
   close,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
-  carrent,
-  jobit,
-  tripguide,
   contacts_pic,
   faq_pic,
   discord_icon,
